9957171488cb4593bf9049db3994b932a24b87d6
freeman <iviewn@gmail.com>
2022-12-08 19:59:12 +0300

    index

--- js/custom.js
+++ js/custom.js
@@ -85,16 +85,20 @@ function remove_hash_from_url() {
             document.title, clean_uri);
     }
 }
-document.addEventListener("mousemove", parallax);
-function parallax(event) {
-    this.querySelectorAll(".move").forEach((shift) => {
-        const position = shift.getAttribute("value");
-        const x = (window.innerWidth - event.pageX * position) / 400;
-        const y = (window.innerHeight - event.pageY * position) / 100;
 
-        shift.style.transform = `translateX(${x}px) translateY(${y}px)`;
-    });
+if ($(window).width() > 1280) {
+    document.addEventListener("mousemove", parallax);
+    function parallax(event) {
+        this.querySelectorAll(".move").forEach((shift) => {
+            const position = shift.getAttribute("value");
+            const x = (window.innerWidth - event.pageX * position) / 400;
+            const y = (window.innerHeight - event.pageY * position) / 100;
+
+            shift.style.transform = `translateX(${x}px) translateY(${y}px)`;
+        });
+    }
 }
+
 
 
 
